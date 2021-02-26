f5c2fc5c845de0cd10f8a85856bfa4897a7cc0d1
BurakEs <burakesen714@gmail.com>
2021-02-27 02:15:10 +0300

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,18 @@
+﻿using Business.Concrete;
+using DataAccess.Concete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var p in productManager.GetByUnitPrice(10,20))
+            {
+                Console.WriteLine(p.ProductName);
+            }
+        }
+    }
+}
---
+++ DataAccess/Concete/EntityFramework/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concete.EntityFramework
+{
+    public class NorthwindContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DEVELOPER;Database=Northwind;Trusted_Connection=True");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
